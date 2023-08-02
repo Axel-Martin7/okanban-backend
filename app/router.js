4264563e44068f31904f759dd63218e44475b887
axel-martin7 <axel.martin@outlook.com>
2023-08-02 18:00:47 +0200

    feat: add cards routes controllers and method into router file

--- app/router.js
+++ app/router.js
@@ -1,7 +1,9 @@
 const express = require("express"); //                              charge express.
 
 /*--------------- Controllers ---------------*/
-const listController = require("./controllers/listController"); //  apelle notre futur controller
+const listController = require("./controllers/listController"); //  appel notre futur controller
+const cardController = require("./controllers/cardController");
+const tagController = require("./controllers/tagController");
 
 /*--------------- Routes ---------------*/
 const router = express.Router(); //                                 charge mon routeur.
@@ -19,8 +21,14 @@ router.put("/lists/:id", listController.modifyList); //             Modifie une 
 router.delete("/lists/:id", listController.deleteList); //          Supprime une liste.
 
 /* Cards */
+router.get("/lists/:id/cards", cardController.getCardsInList); //   Récupère toutes les cartes d'une liste.
+router.get("/cards/:id", cardController.getOneCard); //             Récupère une carte.
+router.post("/cards", cardController.createCard); //                Crée une carte.
+router.put("/cards/:id", cardController.modifyCard); //             Modifie une carte.
+router.delete("/cards/:id", cardController.deleteCard); //          Supprime une carte.
 
 /* Tags */
+router.get("/tags", tagController.getAllTags);
 
 /* Export */
 module.exports = router;
